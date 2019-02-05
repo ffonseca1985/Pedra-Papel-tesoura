8d851547a11ae1a8f27a0c5f7e7203bcdcac7ed1
Fábio Rodrigues Fonseca <ffonseca1985@gmail.com>
2019-02-05 18:06:00 -0200

    add jenkinsFile

---
+++ Btg.TestePratico.PedraPapelTesoura.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Btg.TestePratico.PedraPapelTesoura.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreSame(1, 0);
+        }
+    }
+}
